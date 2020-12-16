6beab828e3faefb5c5f64e3f341eb8a0f9d79ff3
Gerd Gruenert <ggruenert@inovex.de>
2020-12-16 22:00:53 +0100

    Tidy up 13b

--- 13/bus2.hs
+++ 13/bus2.hs
@@ -2,9 +2,10 @@ import Data.List.Split (splitOn)
 import Data.List (minimum)
 
 busraw = "13,x,x,x,x,x,x,37,x,x,x,x,x,461,x,x,x,x,x,x,x,x,x,x,x,x,x,17,x,x,x,x,19,x,x,x,x,x,x,x,x,x,29,x,739,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,x,x,x,23"
--- busraw = "13,x,x,x,x,x,x,37,x,x,x,x,x,461,x,x,x,x,x,x,x,x,x,x,x,x,x,17"
+--busraw = "13,x,x,x,x,x,x,37,x,x,x,x,x,461,x,x,x,x,x,x,x,x,x,x,x,x,x,17"
 --busraw = "7,13,x,x,59,x,31,19"
--- busraw = "1,3,5"
+--busraw = "1,3,5"
+--busraw = "3,5,7"
 
 buslines_str = splitOn "," busraw
 
@@ -34,14 +35,12 @@ naive_times = [(t, all_bus_corret present_buslines t) |
             my_maxnum = maxnum present_buslines
 
 -- smarter, iterative approach:
-
-
 solution_time (bushead:bustail)
     | bustail == []    = bid_next - off_next 
     | otherwise        = t_next
     where 
         t_tail = solution_time bustail
-        prod_tail = product [bid | (Bus bid off) <- bustail]
+        prod_tail = product [bid | (Bus bid _) <- bustail]
         bid_next = bid bushead
         off_next = offset bushead
         t_next = minimum [t | k <- [0..bid_next],
@@ -55,28 +54,21 @@ bus_correct_time (Bus bid off) t = (t + off) `mod` bid == 0
 all_bus_corret blist t = False `notElem` [bus_correct_time bus t | bus <- blist]
 
 
--- main = print $ bus_correct_time (present_buslines !! 3) 123456
--- main = print $ all_bus_corret present_buslines 123456
--- main = print $ all_bus_corret present_buslines 1068781
 main = do
     putStrLn $ "Present buslines: " ++ show present_buslines
     putStrLn $ "Maxnum " ++ show (maxnum present_buslines)
     -- putStrLn $ "Naive solution times: " ++ show naive_times
 
-    putStrLn $ "bus bid: " ++ show (bid (Bus 12 13))
-    putStrLn $ "bus offset: " ++ show (offset (Bus 12 13))
-    putStrLn $ "min: " ++ show ( minimum [5, 2, 4] )
-
     let soltime = solution_time present_buslines
     putStrLn $ "sol time: " ++ show soltime
     putStrLn $ "sol time: " ++ show (all_bus_corret present_buslines soltime)
+
 
 {-
 
 busses 3,5,7
 
-r = 3*5*7
-(i*3)-1 == (j*5)-2 == (k*7)-3
+maxnum = 3*5*7
 
     3   5   7
 0   x   x   x
@@ -96,5 +88,6 @@ r = 3*5*7
 14          x
 15  x   x
 16
+...
 
 -}
