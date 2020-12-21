fc5d78f789083f6992f38004dc857b6cb865299e
Gerd Gruenert <ggruenert@inovex.de>
2020-12-21 18:09:37 +0100

    Almost working - but final-bracket-bug

--- 18/math2.hs
+++ 18/math2.hs
@@ -4,8 +4,8 @@ import Data.List.Split (splitOn)
 import Data.Char (isNumber, ord)
 
 -- switch between trace logging and no verbose output
-trace _ fn = fn 
---import Debug.Trace (trace)
+--trace _ fn = fn 
+import Debug.Trace (trace)
 
 {-
   new approach:
@@ -19,12 +19,73 @@ trace _ fn = fn
 
 -}
 
+
+-- main = print $ parseStringExpression "(3+(4+5))+(6*2)"
+-- main = print $ parseStringExpression "3+4+5+6+2"
+
+
+-- main = print $ use_op (EMLit 5) (EMOp '+') (EMExpr (parseStringExpression "3+4+5+6+2"))
+-- 
+-- main = print $ simplify_expr_4_op (EMOp '+') (EMExpr [EMLit 5])
+-- main = print $ simplify_expr_4_op (EMOp '+') (EMLit 5)
+-- main = print $ simplify_expr_4_op (EMOp '+') (EMExpr [EMLit 5, EMOp '*', EMLit 7, EMOp '+', EMLit 9])
+-- main = print $ simplify_expr_4_op (EMOp '*') (EMExpr [EMLit 5, EMOp '*', EMLit 7, EMOp '+', EMLit 9])
+-- example = parseStringExpression "2+(3+(4+5))+(6+2)+2"
+example = parseStringExpression "2+3+(4+5+6+2+2)"
+main = print $ simplify_expr_4_op (EMOp '+') (EMExpr example)
+--main = print $ example
+
+--            A,+,B,*,C,+,D
+--            l o r rest---
+-- try with:  -----
+-- recurse A:  rec(lor ++ rest)             | op_applicable
+-- recurse B:  l ++ rec(r ++ rest)         | ! op_applicable
+
+
+simplify_expr_4_op :: ExprMem -> ExprMem -> ExprMem
+simplify_expr_4_op _ (EMLit v) = EMLit v
+simplify_expr_4_op op (EMExpr [EMLit lit]) = EMLit lit
+--simplify_expr_4_op op (EMExpr [em]) = simplify_expr_4_op op em    -- not sure if this is a good idea...
+simplify_expr_4_op op (EMExpr expr) = trace ("remaining=" ++ show remaining ++ " from " ++ show expr ++ " op_result=" ++ show op_result ++ " applicable=" ++ show dbg_applicable) 
+                                      $ remaining
+    where
+        l       = head expr
+        myop    = head (tail expr)
+        r       = head (tail (tail expr))
+        rest    = tail (tail (tail expr))
+        l_ex    = simplify_expr_4_op op l
+        r_ex    = simplify_expr_4_op op r
+
+        op_applicable (EMLit _) (EMLit _)    = op == myop
+        op_applicable _ _                    = False
+        op_result = use_op l_ex op r_ex
+        
+        dbg_applicable = op_applicable l r
+
+        -- recurse
+        remaining 
+            | op_applicable l r   = simplify_expr_4_op op $ EMExpr (op_result ++ rest)
+            | otherwise           = join_EMExpr  (EMExpr ([l] ++ [myop]))  $ simplify_expr_4_op op (EMExpr (tail (tail expr))) 
+
+use_op :: ExprMem -> ExprMem -> ExprMem -> [ExprMem]
+use_op (EMLit l) (EMOp '+') (EMLit r) = [EMLit (l+r)]
+use_op (EMLit l) (EMOp '*') (EMLit r) = [EMLit (l*r)]
+-- use_op l op r = [l, op, r]
+
+join_EMExpr (EMExpr ea) (EMExpr eb) = EMExpr (ea ++ eb)
+join_EMExpr (EMExpr ea) (EMLit lit) = EMExpr (ea ++ [EMLit lit])
+join_EMExpr a b = trace ("Uncommon join: " ++ show a ++ " || " ++ show b) $ EMExpr [EMLit 999]
+
+op_applicable (EMLit _) (EMLit _) = True
+op_applicable _ _ = False
+
+
 data ExprMem = EMOp Char | EMLit Int | EMExpr [ExprMem]
-            deriving (Eq, Read)  
-instance Show ExprMem where
-    show (EMOp op) = show op
-    show (EMLit lit) = show lit
-    show (EMExpr ex) = show ex
+            deriving (Eq, Show, Read)  
+--instance Show ExprMem where
+--    show (EMOp op) = show op
+--    show (EMLit lit) = show lit
+--    show (EMExpr ex) = show ex
 
 
 
@@ -67,9 +128,6 @@ extract_parenthesis_fromleft inp = (ls,rs)
         (lsraw,rs,_) = extract_parenthesis_fromleft_rec "(" (tail inp) 1
 
 
-main = print $ parseStringExpression "(3+(4+5))+6*2"
-
-
 {-
 main = do  
     let list = []
@@ -108,14 +166,7 @@ main = do
 tochars inp = [c | c <- inp,
                    c /= ' ']
 do_parsing inp = map tochars (splitOn "\n" inp)
-
-
 c2int c
     | c `elem` ['0'..'9']     = ord c - ord '0'
     | otherwise               = error ("Bad char for conversion to int: " ++ show c)
-
-c2op c opA opB = case c of
-    '+' -> opA + opB
-    '*' -> opA * opB
-    otherwise -> error ("Strange operator character: " ++ [c])
 
